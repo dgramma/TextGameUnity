33ec12f7de801b30ff0e396b15afb5aba800f912
Dave Grammas <dave.grammas@keypathedu.com>
2020-09-25 13:45:24 -0500

    initial commit

---
+++ Assets/AdventureGame.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdventureGame : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = "Now that you're awake, welcome to Gadgetsville. What is Gadgetsville? Where is Gadgetsville? Don't worry about those questions. The only thing you need to know is you aren't leaving this room alive.";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
